f57b6744ca746d07348982dc4ae462044d1e4547
CAREER_SH\mia.meng <2993786681@qq.com>
2021-01-20 19:19:19 +0800

    优化登录相关接口-login方法动态获取验证码

--- AgentUser/login.py
+++ AgentUser/login.py
@@ -9,8 +9,8 @@ class Login(BaseApi):
     # todo:验证码的测试用例后续再写
     def send_phone_code(self):
         # 根据请求的传参获取sign
+        # todo 后面再进一步封装这个方法
         timestamp = int(round(time.time() * 1000))
-        sign_init = ''
         params = {
             "version": 100,
             "productname": "51mdd_agent_pc",
@@ -18,16 +18,15 @@ class Login(BaseApi):
             "key": '',
             "timestamp": timestamp,
             "source": "pc",
-            "sign": sign_init
+            "sign": ''
         }
         data = {
             "mobile": 18275691113
         }
-        param = {**params, **data}
-        del param["sign"]
-        print(param)
-        sign = self.get_sign(param)
+        # 获取sign
+        sign = self.get_sign(params, data)
         params["sign"] = sign
+        # 发送请求
         r = requests.post("http://shanpinapi.51job.com/user/send_phone_code.php",
                           params=params,
                           data=data
@@ -39,23 +38,36 @@ class Login(BaseApi):
         return r
 
     def login_agent(self):
+        # todo:已经登录过，再执行就会报错，要进行数据清理，后续再完成
+        timestamp = int(round(time.time() * 1000))
+        params = {
+            "version": 100,
+            "productname": "51mdd_agent_pc",
+            "brokerid": '',
+            "key": '',
+            "timestamp": timestamp,
+            "source": "pc",
+            "sign": ''
+        }
+        # 获取验证码
+        phonenum = 18275691113
+        verifycode = super().get_verifycode(str(phonenum))
+        data = {
+            "mobile": phonenum,
+            "phonecode": verifycode,
+            "source": "pc"
+        }
+        # 获取sign
+        sign = self.get_sign(params, data)
+        params["sign"] = sign
+        # 发送请求
         r = requests.post("http://shanpinapi.51job.com/user/login.php",
-                          params={
-                              "version": 100,
-                              "productname": "51mdd_agent_pc",
-                              "brokerid": '',
-                              "key": '',
-                              "timestamp": 1611131029593,
-                              "source": "pc",
-                              "sign": "968ba9acf47cf16da9de2a18ecfd1cde"
-                          },
-                          data={
-                              "mobile": 18275691113,
-                              "phonecode": 152938,
-                              "source": "pc",
-                          }
+                          params=params,
+                          data=data
         )
         print(json.dumps(r.json(), ensure_ascii=False, indent=2))
+        assert r.status_code == 200
+        assert r.json()['status'] == 1
         return r
 
     def logout_agent(self):
