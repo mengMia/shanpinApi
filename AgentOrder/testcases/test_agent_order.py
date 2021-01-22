5b93f3b26df34a9ba5735daf0e58314b2815fe0e
CAREER_SH\mia.meng <2993786681@qq.com>
2021-01-22 13:34:42 +0800

    修改package名称

--- AgentOrder/testcases/test_agent_order.py
+++
@@ -1,9 +0,0 @@
-from AgentOrder.agent_order import AgentOrder
-
-
-class TestAgentOrder():
-    def setup(self):
-        self.order = AgentOrder()
-
-    def test_get_order_list(self):
-        self.order.get_order_list()
