64775345fa7008040543ff4ca6949e19c483365d
CAREER_SH\mia.meng <2993786681@qq.com>
2021-01-22 10:25:25 +0800

    测试cookie的获取

--- AgentUser/method_test.py
+++ AgentUser/method_test.py
@@ -49,7 +49,7 @@ def get_time():
 def save_cookie():
     # todo：这里先用shelve保存写死的cookie，然后获取, cookie可能会过期，或者可以从redis中获取cookie
     # 从数据库中取出cookie
-    db = shelve.open("datas/cookies")
+    db = shelve.open("../datas/cookies")
     cookie = "3f8aaaed5a7e57d70ad30e59ce0315a9"
     db['cookie'] = cookie
     db.close()
