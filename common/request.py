204fd601c2a2a6b002890144d47fc67f7d9b7eb2
CAREER_SH\mia.meng <2993786681@qq.com>
2021-01-23 15:12:52 +0800

    封装一个主request方法，是被调用的，然后根据传入的method，调用post或者get

--- common/request.py
+++ common/request.py
@@ -6,11 +6,34 @@ class RunMethod():
     request方法
     """
     def post_main(self, url, param, data):
+        """
+        post请求
+        """
         r = requests.post(url=url, params=param, data=data)
         return r
 
     def get_main(self, url, param):
+        """
+        get请求
+        """
         r = requests.get(url=url, params=param)
         return r
+
+    def run_main(self, method, url, params=None, data=None):
+        """
+        被调用主request
+        """
+        try:
+            if method == 'post' or method == 'POST' or method == 'Post':
+                r = self.post_main(url, params, data)
+            elif method == 'get' or method == 'GET' or method == 'Get':
+                r = self.get_main(url, params)
+            else:
+                return "request传参错误"
+            return r
+        except Exception as e:
+            print("请求方法报错")
+
+
 
 
