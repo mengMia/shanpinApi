6862b0ce080aa9aaf30fc1713dff9cad93416e12
CAREER_SH\mia.meng <2993786681@qq.com>
2021-01-23 17:53:15 +0800

    优化登录的功能-登录方法里面调用发送验证码的接口和获取验证码的接口，走通了登录的完整流程

--- common/login.py
+++ common/login.py
@@ -15,34 +15,41 @@ class Login():
     def __init__(self):
         with open("../config/test.yaml", encoding='utf-8') as f:
             self.base_param = yaml.safe_load(f)
+
         # 获取base_url
         self.base_url = self.base_param["shanpinApi"]["base_url"]
         # 获取手机号码
         self.phonenum = self.base_param["shanpinApi"]["account"]["jing_account"]
+        # 获取登录相关的
+        self.login = self.base_param["shanpinApi"]["agent_login"]
         # 获取登录的请求方法
-        self.login_method = self.base_param["shanpinApi"]["agent_login"]["login_method"]
+        self.login_method = self.login["login_method"]
         # 获取登录的url
-        self.login_url = self.base_param["shanpinApi"]["agent_login"]["login_url"]
-        # 获取发送验证码的请求方法
-        self.sms_url = self.base_param["shanpinApi"]["agent_sms_code"]["sms_method"]
-        # 获取发送验证码的url
-        self.sms_url = self.base_param["shanpinApi"]["agent_sms_code"]["sms_url"]
-        # 从数据库获取验证码
-        self.verifycode = self.get_verifycode(str(self.phonenum))
+        self.login_url = self.login["login_url"]
 
+        # 获取验证码相关的参数
+        self.sms = self.base_param["shanpinApi"]["agent_sms_code"]
+        # 获取发送验证码的请求方法
+        self.sms_url = self.sms["sms_method"]
+        # 获取发送验证码的url
+        self.sms_url = self.sms["sms_url"]
 
     def agent_login(self):
         # todo:已经登录过，再执行就会报错，要进行数据清理，后续再完成
 
         # todo : 这个yaml中使用变量的，以后通过其他办法解决，
+        params = self.login["params"]
+        data = self.login["data"]
+        # 获取并替换时间戳
         timestamp = int(round(time.time() * 1000))
-        self.base_param["shanpinApi"]["agent_login"]["params"]["timestamp"] = timestamp
+        params["timestamp"] = timestamp
+
+        # 发送验证码
+        self.send_phone_code()
 
         # 获取并替换掉验证码
         verifycode = self.get_verifycode(str(self.phonenum))
-        self.base_param["shanpinApi"]["agent_login"]["data"]["phonecode"] = verifycode
-        params = self.base_param["shanpinApi"]["agent_login"]["params"]
-        data = self.base_param["shanpinApi"]["agent_login"]["data"]
+        data["phonecode"] = verifycode
 
         # 获取并替换掉sign
         sign = self.sign.get_sign(params, data)
@@ -52,10 +59,32 @@ class Login():
         # 发送请求
         r = self.request.run_main(self.login_method, url, params, data)
         print(json.dumps(r.json(), ensure_ascii=False, indent=2))
+        return r
+
+    def send_phone_code(self):
+        # 根据请求的传参获取sign
+        # todo 后面再进一步封装这个方法
+        params = self.login["params"]
+        data = self.sms["data"]
+        # 获取并替换时间戳
+        timestamp = int(round(time.time() * 1000))
+        params["timestamp"] = timestamp
+
+        # 获取并替换掉sign
+        sign = self.sign.get_sign(params, data)
+        params["sign"] = sign
+        url = self.base_url + self.sms_url
+
+        # 发送请求
+        r = self.request.post_main(url, params, data)
+
+        print(json.dumps(r.json(), ensure_ascii=False, indent=2))
+        assert r.status_code == 200
+        assert r.json()['status'] == 1
+        return r
 
     def get_verifycode(self, phonenum):
         # done:手机号码作为变量传入
-        # todo:不知道从数据库获取验证码的方式是否符合普通规则，毕竟线上是没法从数据库获取验证码的，上线之后怎么用这个用例验证呢
         # 调用conn_db方法获取conn
         self.cursor = self.db.conn_db().cursor()
         self.cursor.execute('select top 1 content from smssendqueue where KeyNum = %s order by smsid desc',
@@ -68,5 +97,6 @@ class Login():
 if __name__ == '__main__':
     test = Login()
     test.agent_login()
+    # test.send_phone_code()
 
 
