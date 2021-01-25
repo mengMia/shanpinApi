38a6ce9e97d168b91c3f1d182faea8aad351cf07
CAREER_SH\mia.meng <2993786681@qq.com>
2021-01-25 18:57:28 +0800

    打算添加变量替换

--- common/sign.py
+++ common/sign.py
@@ -1,12 +1,19 @@
 from hashlib import md5
 
+from common.read_file import ReadFile
+
 
 class Sign():
+    file = ReadFile()
     def get_sign(self, param_request, data_request):
         """
         根据传入的参数获取签名，更新请求的params
         """
         # logging.info("调用了getSign方法")
+        # # 先对从yaml中传进来的param_request, data_request进行变量替换
+        # param_request = self.file.var_replace(param_request, params)
+
+
         # 合并请求里的params和data传参
         param = {**param_request, **data_request}
         del param["sign"]
@@ -25,6 +32,5 @@ class Sign():
         # md5加密
         key = "51job-signature-agent"
         token_md5 = (md5(query.encode(encoding='UTF-8')).hexdigest()) + key
-        # print(token_md5)
-        token = md5(token_md5.encode(encoding='UTF-8')).hexdigest()
-        return token
+        sign = md5(token_md5.encode(encoding='UTF-8')).hexdigest()
+        return sign
