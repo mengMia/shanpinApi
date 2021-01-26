085ca92b96471dae20397270038170c486479821
CAREER_SH\mia.meng <2993786681@qq.com>
2021-01-26 16:42:15 +0800

    获取招聘订单列表接口数据驱动，并移动到api文件夹

--- api/agent_order.py
+++ api/agent_order.py
@@ -4,40 +4,38 @@ import time
 import requests
 
 from common.base_api import BaseApi
+from common.get_sign import Sign
+from common.read_file import ReadFile
+from common import request
 
 
 class AgentOrder(BaseApi):
-    def get_order_list(self):
+    sign = Sign()
+    request = request.RunMethod()
+    file = ReadFile()
+    base_param = file.read_yaml("test_yaml_path")["shanpinApi"]["base_params"]
+    order_param = file.read_yaml("order_yaml_path")["shanpinApi"]["order"]
+    rep_params = {}
+    def get_order_list(self, keyword, **kwargs):
         """
         获取所有招聘订单列表
         :return:
         """
         timestamp = int(round(time.time() * 1000))
         key = self.get_cookie()
-        params = {
-            "pageno":1,
-            "pagesize": 12,
-            "keyword": '',
-            "jobarea": '',
-            "funtype": '',
-            "sorttype": 0,
-            "version": 100,
-            "productname": "51mdd_agent_pc",
-            "brokerid": 87,
-            "key": key,
-            "timestamp": timestamp,
-            "source": "pc",
-            "sign": ''
-        }
-        data = {
-        }
-        # 获取sign
-        sign = self.get_sign(params, data)
-        params["sign"] = sign
+        self.rep_params["timestamp"] = str(timestamp)
+        self.rep_params["key"] = key
+        self.rep_params["keyword"] = keyword
+        self.rep_params["brokerid"] = '85'
+        addi_params = self.order_param["list"]['addi_params']
 
-        r = requests.get("http://shanpinapi.51job.com/order/get_order_list.php",
-                          params=params,
-                          )
+        params = {**self.base_param, **addi_params}
+        params = self.sign.replace_sign(params, self.rep_params)
+
+        method = self.order_param["list"]["method"]
+        url = self.order_param["list"]["url"]
+
+        r = self.request.run_main(method, url, params, self.rep_params)
         print((json.dumps(r.json(), indent=2, ensure_ascii=False)))
         assert r.status_code == 200
         assert r.json()['status'] == 1
