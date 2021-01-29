0ca6b198dabefaa1e67c18ae6c35b6c01dd40068
CAREER_SH\mia.meng <2993786681@qq.com>
2021-01-29 19:55:39 +0800

    重新修改传参方式，需要一个参数一个参数的列出来，传进去--待优化

--- testcases/api_test/test_agent_order.py
+++ testcases/api_test/test_agent_order.py
@@ -1,18 +1,26 @@
 import pytest
 
 from api.agent_order import AgentOrder
+from common.read_file import ReadFile
+
 
 class TestAgentOrder():
-    # def setup(self):
+    # todo:难道每个测试用例都要传入key进去吗
     order = AgentOrder()
+    file = ReadFile()
+    api_data = file.read_yaml("order_yaml_path")["shanpinApi"]["order"]["list"]
+    # print(type(api_data["test_keyword0"]))
+    # data = api_data["test_keyword0"]
 
-    # @pytest.mark.parametrize("keyword", api_data["test_keyword0"]["keyword"])
-    def test_get_order_list(self, get_token):
+    @pytest.mark.parametrize('pageno, pagesize, keyword, jobarea, funtype, sorttype', [
+        api_data["test_keyword0"]])
+    def test_get_order_list(self, get_token, pageno, pagesize, keyword, jobarea, funtype, sorttype):
         key = get_token
-        self.order.get_order_list(key)
+        self.order.get_order_list(key, pageno, pagesize, keyword, jobarea, funtype, sorttype)
 
-    def test_get_job_info(self):
-        self.order.get_job_info()
+    def test_get_job_info(self, get_token):
+        key = get_token
+        self.order.get_job_info(key)
 
     def test_get_share_image(self):
         self.order.get_share_image()
@@ -20,6 +28,6 @@ class TestAgentOrder():
     def test_set_collection(self):
         self.order.set_collection()
 
-# if __name__ == '__main__':
-#     test = TestAgentOrder()
-#     test.get_data()
+if __name__ == '__main__':
+    test = TestAgentOrder()
+    # test.get_data()
