9611e679e41207925b8031493559060663e67c56
CAREER_SH\mia.meng <2993786681@qq.com>
2021-01-30 13:59:50 +0800

    添加fixture-获取测试用例参数

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -1,7 +1,5 @@
 import json
-
 import pytest
-
 from common.login import Login
 from common.read_file import ReadFile
 
@@ -16,5 +14,16 @@ def get_token():
     get_token = r.json()['key']
     return get_token
 
-file = ReadFile()
-api_data = file.read_yaml("order_yaml_path")["shanpinApi"]["order"]["list"]
+@pytest.fixture(scope="function")
+def get_testcases(request):
+    # 通过request.param获取参数
+    test_cases = request.param
+    # print(f"\n 登录用户：{user}")
+    return test_cases
+
+# # 这个暂时没用，但是执行测试用例时会去加载D:\CS\PythonCode\testcases\api_test\conftest.py文件，这个文件中有导入api_data的语句，如果不写的话会报错
+# # 可见执行测试用例的时候，即使不引用conftest，也会去先加载conftest文件
+# file = ReadFile()
+# api_data = file.read_yaml("order_yaml_path")["shanpinApi"]["order"]["list"]
+
+
