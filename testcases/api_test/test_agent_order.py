8f786a9f47ed53dee7432bde49a5d18dd1b23dc4
CAREER_SH\mia.meng <2993786681@qq.com>
2021-01-30 14:00:59 +0800

    @pytest.mark.parametrize中的传入参数设置为获取用例的函数名，value设置为用例数据（list），只需要传入一个参数就行了

--- testcases/api_test/test_agent_order.py
+++ testcases/api_test/test_agent_order.py
@@ -2,19 +2,21 @@ import pytest
 
 from api.agent_order import AgentOrder
 from common.read_file import ReadFile
+from testcases.conftest import get_testcases
 
 
 class TestAgentOrder():
     # todo:难道每个测试用例都要传入key进去吗
     order = AgentOrder()
     file = ReadFile()
-    api_data = file.read_yaml("order_yaml_path")["shanpinApi"]["order"]["list"]
+    api_data = file.read_yaml("order_yaml_path")["shanpinApi"]["order"]["get_order_list"]
+    print(api_data["test_cases"])
 
-    @pytest.mark.parametrize('pageno, pagesize, keyword, jobarea, funtype, sorttype',
-        api_data["test_cases"])
-    def test_get_order_list(self, get_token, pageno, pagesize, keyword, jobarea, funtype, sorttype):
+    @pytest.mark.parametrize('get_testcases', api_data["test_cases"], indirect=True)
+    def test_get_order_list(self, get_token, get_testcases):
         key = get_token
-        self.order.get_order_list(key, pageno, pagesize, keyword, jobarea, funtype, sorttype)
+        test_cases = get_testcases
+        self.order.get_order_list(key, test_cases)
 
     def test_get_job_info(self, get_token):
         key = get_token
