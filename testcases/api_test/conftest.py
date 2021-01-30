ab463907358f66f575995ae0b7a11714a6dacd02
CAREER_SH\mia.meng <2993786681@qq.com>
2021-01-30 13:59:10 +0800

    暂时注释掉这些内容

--- testcases/api_test/conftest.py
+++ testcases/api_test/conftest.py
@@ -1,11 +1,11 @@
-import pytest
-
-from testcases.conftest import api_data
-
-
-@pytest.fixture(scope="function")
-def testcase_data(request):
-    print("----api_test/conftest----")
-    testcase_name = request.function.__name__
-    print(testcase_name)
-    return api_data.get(testcase_name)
+# import pytest
+#
+# from testcases.conftest import api_data
+#
+#
+# @pytest.fixture(scope="function")
+# def testcase_data(request):
+#     print("----api_test/conftest----")
+#     testcase_name = request.function.__name__
+#     print(testcase_name)
+#     return api_data.get(testcase_name)
