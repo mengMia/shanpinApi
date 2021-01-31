6194de56ea8f50dc026b541adff67a8148dbe27b
CAREER_SH\mia.meng <2993786681@qq.com>
2021-01-31 14:14:42 +0800

    参数化不需要用fixture下的函数来获取一个list，直接参数设置为一个，传入一个list就行了，再通过自己写的方法处理成字典

--- testcases/api_test/test_agent_order.py
+++ testcases/api_test/test_agent_order.py
@@ -21,13 +21,12 @@ class TestAgentOrder():
     # print(api_data["test_cases"])
 
     @allure.story("用例--登录状态下获取订单列表")
-    @pytest.mark.parametrize('get_testcases', api_data["get_order_list"]["test_cases"], indirect=True)
-    def test_get_order_list(self, get_token, get_testcases):
+    # @pytest.mark.parametrize('get_testcases', api_data["get_order_list"]["test_cases"], indirect=True)
+    @pytest.mark.parametrize('test_cases', api_data["get_order_list"]["test_cases"])
+    def test_get_order_list(self, get_token, test_cases):
         # todo:订单列表，是否登录，获取的列表不一样，用例还没设计
         # 通过传入函数名get_token调用fixture来获取登录key
         key = get_token
-        # 通过传入函数名调用fixture来获取测试用例
-        test_cases = get_testcases
         # 获取测试用例的参数之后，要把base_params进行变量替换，获取sign之后再传入接口
         brokerid = '85'
         params = self.param.get_requestparams(brokerid, key, test_cases, self.base_param, self.api_data["get_order_list"]["params"])
