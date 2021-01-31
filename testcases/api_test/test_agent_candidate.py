ef16e317ba28446f675fd96d7232bb2a323e1e92
CAREER_SH\mia.meng <2993786681@qq.com>
2021-01-31 17:03:58 +0800

    暂时先只测试上传和推荐简历的吧

--- testcases/api_test/test_agent_candidate.py
+++ testcases/api_test/test_agent_candidate.py
@@ -8,17 +8,17 @@ class TestAgentCandidate():
     def test_recommend_resume(self):
         self.candidate.recommend_resume()
 
-    def test_check_has_resume(self):
-        self.candidate.check_has_resume()
-
-    def test_get_candidate_list(self):
-        self.candidate.get_candidate_list()
-
-    def test_get_resume_detail(self):
-        self.candidate.get_resume_detail()
-
-    def test_remind_stage_audit(self):
-        self.candidate.remind_stage_audit()
+    # def test_check_has_resume(self):
+    #     self.candidate.check_has_resume()
+    #
+    # def test_get_candidate_list(self):
+    #     self.candidate.get_candidate_list()
+    #
+    # def test_get_resume_detail(self):
+    #     self.candidate.get_resume_detail()
+    #
+    # def test_remind_stage_audit(self):
+    #     self.candidate.remind_stage_audit()
 
     def test_upload_photo(self):
         self.candidate.upload_photo()
