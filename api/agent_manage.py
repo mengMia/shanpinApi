12af8b9890a25a8c2bf888ceeca692162e8f4f88
CAREER_SH\mia.meng <2993786681@qq.com>
2021-02-02 15:00:08 +0800

    添加经纪人-数据驱动

--- api/agent_manage.py
+++ api/agent_manage.py
@@ -4,40 +4,24 @@ import time
 import requests
 
 from common.base_api import BaseApi
+from common.read_file import ReadFile
+from common import request
 
 
 class AgentManage(BaseApi):
-    def add_agent(self):
+    request = request.RunMethod()
+    file = ReadFile()
+    base_param = file.read_yaml("test_yaml_path")["shanpinApi"]["base_params"]
+    user_param = file.read_yaml("user_yaml_path")["shanpinApi"]["user"]
+    def add_agent(self, params, data):
         """
         添加经纪人
         :return:
         """
-        timestamp = int(round(time.time() * 1000))
-        key = self.get_cookie()
-        params = {
-            "version": 100,
-            "productname": "51mdd_agent_pc",
-            "brokerid": 87,
-            "key": key,
-            "timestamp": timestamp,
-            "source": "pc",
-            "sign": ''
-        }
-        data = {
-            "phonenum": 19122220003,
-            "name": "测试",
-        }
-        # header = {
-        #     "Content-Type": 'application/json'
-        # }
-        # 获取sign
-        sign = self.get_sign(params, data)
-        params["sign"] = sign
-
-        r = requests.post("https://shanpinapi.51job.com/user/add_broker.php",
-                          params=params,
-                          data=data)
-        # r.encoding = "gbk"
+        # 获取请求方式和url
+        method = self.user_param["add_agent"]["method"]
+        url = self.user_param["add_agent"]["url"]
+        r = self.request.run_main(method, url, params, data)
         print((json.dumps(r.json(), indent=2, ensure_ascii=False)))
         assert r.status_code == 200
         assert r.json()['status'] == 1
@@ -80,39 +64,39 @@ class AgentManage(BaseApi):
         assert r.json()['result'] == 1
         return r
 
-    def get_agent_list(self):
-        """
-        获取经纪人列表
-        :return:
-        """
-        timestamp = int(round(time.time() * 1000))
-        key = self.get_cookie()
-        params = {
-            "version": 100,
-            "productname": "51mdd_agent_pc",
-            "brokerid": 87,
-            "key": key,
-            "timestamp": timestamp,
-            "source": "pc",
-            "sign": ''
-        }
-        data = {
-            "pageno": 1,
-            "pagesize": 10
-        }
-        # 获取sign
-        sign = self.get_sign(params, data)
-        params["sign"] = sign
-
-        r = requests.post("http://shanpinapi.51job.com/user/get_broker_list.php",
-                          params=params,
-                          data=data)
-        # r.encoding = "gbk"
-        print((json.dumps(r.json(), indent=2, ensure_ascii=False)))
-        assert r.status_code == 200
-        assert r.json()['status'] == 1
-        assert r.json()['result'] == 1
-        return r
+    # def get_agent_list(self):
+    #     """
+    #     获取经纪人列表
+    #     :return:
+    #     """
+    #     timestamp = int(round(time.time() * 1000))
+    #     key = self.get_cookie()
+    #     params = {
+    #         "version": 100,
+    #         "productname": "51mdd_agent_pc",
+    #         "brokerid": 87,
+    #         "key": key,
+    #         "timestamp": timestamp,
+    #         "source": "pc",
+    #         "sign": ''
+    #     }
+    #     data = {
+    #         "pageno": 1,
+    #         "pagesize": 10
+    #     }
+    #     # 获取sign
+    #     sign = self.get_sign(params, data)
+    #     params["sign"] = sign
+    #
+    #     r = requests.post("http://shanpinapi.51job.com/user/get_broker_list.php",
+    #                       params=params,
+    #                       data=data)
+    #     # r.encoding = "gbk"
+    #     print((json.dumps(r.json(), indent=2, ensure_ascii=False)))
+    #     assert r.status_code == 200
+    #     assert r.json()['status'] == 1
+    #     assert r.json()['result'] == 1
+    #     return r
 
     def change_agent_status(self):
         """
