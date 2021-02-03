19bfd0a87df4b44bd40dadbd2c8f15dc5fbb99dc
CAREER_SH\mia.meng <2993786681@qq.com>
2021-02-03 09:36:18 +0800

    这个测试用例在fixture（delete_agent）中要用到，所以使用fixture函数获取测试用例，两边都可以用到

--- testcases/api_test/test_agent_user.py
+++ testcases/api_test/test_agent_user.py
@@ -15,24 +15,30 @@ class TestUser:
     api_data = file.read_yaml("user_yaml_path")["shanpinApi"]["user"]
     # todo：可以放在conftest文件中吧
     base_param = file.read_yaml("test_yaml_path")["shanpinApi"]["base_params"]
+
     # test_cases = api_data["add_agent"]["test_cases"]
 
     # print(api_data["test_cases"])
 
-    # @allure.story("用例--登录状态下获取订单列表")
-
-    @pytest.mark.parametrize('test_cases', api_data["add_agent"]["test_cases"],
-                             ids=api_data["add_agent"]["ids"])
-    def test_add_agent(self, get_token, test_cases):
+    @allure.story("用例--添加经纪人")
+    @pytest.mark.parametrize('delete_agent', api_data["add_agent"]["test_cases"],
+                             ids=api_data["add_agent"]["ids"], indirect=True)
+    def test_add_agent(self, get_token, delete_agent):
         key = get_token
         # 获取测试用例的参数之后，要把base_params进行变量替换，获取sign之后再传入接口
         brokerid = '85'
+        test_cases = delete_agent
         [query, data] = self.param.get_requestparams(brokerid, key, test_cases, self.base_param,
                                               self.api_data["add_agent"]["params"])
         r = self.user.add_agent(query, data)
         expect_result = self.api_data["add_agent"]["expect_result"]
         assert r.status_code == expect_result[0]
         self.log.info("status ==> 期望结果:{}, 实际结果：【{}】".format(expect_result[1], r.json()['status']))
+        assert r.json()['status'] == expect_result[1]
+        assert r.json()['result'] == expect_result[2]
+
+
+        self.log.info("数据库更新 ==> 期望结果:{}, 实际结果：【{}】".format(expect_result[1], r.json()['status']))
         assert r.json()['status'] == expect_result[1]
         assert r.json()['result'] == expect_result[2]
         self.log.info("*************测试用例执行结束****************")
